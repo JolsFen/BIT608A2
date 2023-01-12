deeed80abf16d0f1b3d1c3a9a22cf68f1c963973
Jolie <110357567+JolsFen@users.noreply.github.com>
2023-01-12 16:59:41 +1300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<!--Footer section using Flexbox-->
+<div class = "container">
+        <footer>  
+            <img src="..\images\logo.jpg" alt="Motueka B&B Logo" width="100" height="80">
+            <span class="copyright">&copy; 2022 Motueka B&B</span>
+            <span class="Privacy Policy"><a href="..\Privacy Statement.html">Privacy Statement</a></span>
+            <img class="d-inline-block align-bottom" src="..\images\fb.jpg" alt="Facebook Logo" width="50" height="50">
+            <img class="d-inline-block align-bottom" src="..\images\Instagram.jpg" alt="Instagram Logo" width="50" height="50">
+        </footer></div><br><br><!--End of footer-->
