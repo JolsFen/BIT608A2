e6b4701608b7549a4b2d7f4051b1f4b30a18d846
Jolie <110357567+JolsFen@users.noreply.github.com>
2023-01-12 16:59:17 +1300

    Create header.php

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="..\css\Flexbox.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+  </head>
+
+  <nav class="navbar navbar-expand-sm bg-light navbar-light">  <!--Navigation Bar-->
+        <div class="container">
+          <img class="d-inline-block align-top" src='Images\Logo.jpg' alt="The Motueka Bed & Breakfast Logo" width="100" height="70">&emsp;
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+              aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="navbar-nav me-auto mb-2 mb-lg-0">
+              <div class="col-sm">
+                <a class="nav-link" href="index.php" style="text-align:center"><h5>Home</h5></a>
+              </div>
+              <div class="col-sm">
+                <a class="nav-link" href="registercustomer.php"><h5>Register&emsp;</h5></a>
+              </div>
+              <div class="col-sm">
+                <a class="nav-link" href="makeabooking.php" style="text-align:center"><h5>Bookings</h5></a>
+              </div>
+              <div class="col-sm">
+                <a class="nav-link" href="listrooms.php" style="text-align:center"><h5>Rooms</h5></a>
+              </div>
+              <div class="col-sm">
+                <a class="nav-link" href="listcustomers.php" style="text-align:center"><h5>Customers</h5></a>
+              </div>
+              <div class="col-sm">
+              <li class="nav-item active">
+                  <a href="login.php" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Login</a>
+            </div>
+          </div>
+      </nav><br> <!--End of Navigation Bar-->
