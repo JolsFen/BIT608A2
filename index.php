ecbebd6b1d760f9ba4e68377b0e2dc555b85eaa0
Jolie <110357567+JolsFen@users.noreply.github.com>
2023-01-12 15:56:36 +1300

    Create index.php

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Motueka Bed & Breakfast</title> 
+    </head>
+
+<?php
+include "header.php";
+?>
+
+
+<body>
+
+<div class="container">
+        <h1>Motueka Bed & Breakfast Accommodation</h1>
+        <p><strong>15 Kowhai Cres, Motueka (03) 555 666</strong></p>
+        <nav>
+  <a href='makeabooking.php'>[Make Booking]</a>  <!--Link to make a booking-->
+  <a href='listbookings.php'>[View Bookings]</a>   <!--Link to Home Page-->
+  </nav><br>
+
+
+    <p>
+      Welcome to our Motueka Bed & Breakfast, please register by clicking the 'register' link above and make a booking with us! We have many<br>
+      accommodation options to suit your needs.  We are located centrally so we are close to all cafes, restaurants and activities in the area,<br>
+      please browse through our webpage, we look forward to meeting you soon!
+    </p>
+
+        <div class="row">
+          <div class="col-sm">
+            <img src='Images\Motueka1.jpg' alt="The Motueka Bed & Breakfast Logo" width="250" height="200">
+          </div>
+          <div class="col-sm">
+            <img src='Images\Kitchen1.jpg' alt="The Motueka Bed & Breakfast Logo" width="250" height="200">
+          </div>
+          <div class="col-sm">
+            <img src='Images\Motueka6.jpg' alt="The Motueka Bed & Breakfast Logo" width="250" height="200">
+          </div>
+        </div><br><br>
+
+    <h2>Find Us</h2>
+    <p><strong>15 Kowhai Cres, Motueka (03) 555 666</strong></p>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48088.67458972066!2d172.98004795000003!3d-41.1226891!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d3b81309f5a66d9%3A0x500ef8684798ff0!2sMotueka!5e0!3m2!1sen!2snz!4v1673058946853!5m2!1sen!2snz" 
+      width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <br><br>
+
+  <h2>About Us</h2>
+  <img src='Images\hohepa.jpg' alt="The owners" width="300" height="200"><br>
+  <p>We are the Hohepa's, welcome to our home.  We are recently retired and our home is far to large for just the two<br>
+     of us so we would love you to come and stay!  We have many room options to cater to your needs and breakfast is<br>
+     included.  Please check out our webpage, you can book online or please contact our booking manager on (03) 555 666.<br>
+     We look forward to hosting you soon!</p>
+<br>
+
+<h2>What to do in Motueka</h2>
+  <img src='Images\motueka10.jpg' alt="The owners" width="300" height="200"><br>
+  <p>
+    Motueka is a town in the South Island of New Zealand, close to the mouth of the Motueka River on the western<br>
+    shore of Tasman Bay / Te Tai-o-Aorere. It is the second largest in the Tasman Region, with a population of 8,280<br>
+    as of June 2021.</p>
+    <p>
+    Motueka is a vibrant town, known for its lively cafe culture and artistic community.  It is also a gateway town<br>
+    to the Abel Tasman National Park.  To find out more click here: <a href="https://www.newzealand.com/nz/motueka/">Motueka</a>
+    </p>
+</div><br>
+
+<!--Footer section using Flexbox-->
+<?php
+include "footer.php";
+?>
+
+</body>
+</html>
